61ab82f09d53314fcf34583c7cdd3bf1d43b686e
Fih <124883284+Firene11@users.noreply.github.com>
2023-06-01 18:31:05 -0300

    added click event for save button, need to test if works

--- Develop/script.js
+++ Develop/script.js
@@ -4,16 +4,25 @@
 
 $( ".row" ).wrapAll("<div class='time-id' />");
 
+
+// TODO: Add code to display the current date in the header of the page.
 var currentDate = dayjs().format ("dddd-MM-DD-YYYY");
 $("#currentDay").html(currentDate);
 
 var saveButtonEl = $('#saveBtn');
 
-$(function () {
+$(document).ready(function () {
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. 
     
+    $(".saveBtn").on("click", function () {
+        var textArea = $(this).siblings(".description").val();
+        var timeBlock = $(this).parent().attr("id");
+
+        localStorage.setItem(timeBlock, textArea);
+        console.log(timeBlock + textArea);
+        })
 
     //HINT: What does `this` reference in the click listener
     // function? How can DOM traversal be used to get the "hour-x" id of the
@@ -29,7 +38,5 @@ $(function () {
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
   });
   
