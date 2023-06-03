f26ef2f0a4555ab9dd71b12ccb992f11dd53b2b7
Fih <124883284+Firene11@users.noreply.github.com>
2023-06-03 18:50:16 -0300

    cleaned up code and added helpful commenting on what does what in the code

--- Develop/script.js
+++ Develop/script.js
@@ -1,20 +1,23 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+//Wraps all code that interacts with the DOM in a call to jQuery so that the code isn't run until 
+//the browser has finished rendering all the elements in the html.
 
 $( ".row" ).wrapAll("<div class='time-id' />");
 
 
-// TODO: Add code to display the current date in the header of the page.
+//Displays the current date in the header of the page.
+
 var currentDate = dayjs().format ("dddd-MM-DD-YYYY");
 $("#currentDay").html(currentDate);
 
 var saveButtonEl = $('#saveBtn');
 
+
+//Begins the function
 $(document).ready(function () {
 
-//Added a listener for click events on the save button. This code uses the id in the containing time-block 
-//as a key to save the user input in local storage.   
+
+//Listener for click events on save button. Uses the id in the containing time-block to save user input
+//in local storage.   
 
     $(".saveBtn").on("click", function () {
         var textArea = $(this).siblings(".description").val();
@@ -24,14 +27,13 @@ $(document).ready(function () {
         console.log(timeBlock + textArea);
         })
 
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
 
     function timeSections () {
+//Uses Day.js to get the current hour in 24-hour time
         var currentTime = dayjs().format("H");
+
+
+//Applies the past, present, or future class to each time block by comparing the id to the current hour.
 
         $(".time-block").each(function () {
             var workHour = parseInt(this.id);
@@ -46,10 +48,12 @@ $(document).ready(function () {
         })
     }
 
+//Calls the function to work
     timeSections();
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
+
+
+//Saves any user input that was saved in localStorage and sets the values of the corresponding text area 
+//elements. 
 
     $('.time-block').each(function() {
         const key = $(this).attr('id');
@@ -58,5 +62,3 @@ $(document).ready(function () {
       });
 
   });
-timeSections();
-  
